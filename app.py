4d7053b472931f2712b8a5a1bbb7f74ad38ae532
Ruven <ruvenguna94@hotmail.com>
2019-03-26 13:10:04 +0800

    Update 2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests, ast
 
 app = Flask(__name__)
 
-@app.route("/getAccounts")
+@app.route("/getAccounts", methods=["POST"])
 def getAccounts():
     myinput = request.get_json()
     acc_dict = {}
@@ -29,7 +29,7 @@ def getAccounts():
 
 
 
-@app.route("/balance")
+@app.route("/balance", methods=["POST"])
 def balance():
     myinput = request.get_json()
     acc_id = myinput['accountId']
@@ -45,6 +45,20 @@ def balance():
 
     return jsonify(bal_dict)
 
+@app.route("/topTransactions", methods=["POST"])
+def topTransactions():
+    myinput = request.get_json()
+    acc_id = myinput['accountId']
+    endpoint_topTransactions = 'http://api-gateway-dbs-techtrek.ap-southeast-1.elasticbeanstalk.com/transactions/{}?from=01-01-2019&to=01-31-2019'.format(acc_id)
+    headers = {
+        'Content-Type' : 'application/json',
+        'token' : '545a6a5f-f955-48c1-936b-d545eac1aee8',
+        'identity' : 'Group8'
+    }
+    code = requests.get(endpoint_topTransactions, headers=headers)
+    all_transactions = ast.literal_eval(code.text)
+
+    return jsonify(all_transactions)
 
 if __name__ == "__main__":
     app.run(debug=True)
